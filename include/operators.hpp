eb9ab02d9a659d2ba3aa1903e2aee79dd0645f25
malevitzch <malevitzch@gmail.com>
2024-07-05 19:01:56 +0200

    Implemented the classes for both Unary and Binary operators, changed the scheme to use inheritance to promote code reusability

--- include/operators.hpp
+++ include/operators.hpp
@@ -3,26 +3,34 @@
 #include <string>
 using std::string;
 
-class BinaryOperator
+class Operator
 {
 private:
   string name;
+  string symbol;
+protected:
+  Operator(string name, string symbol);
+public:
+  Operator() = delete;
+  string get_name() const;
+  string get_symbol() const;
+};
+class BinaryOperator : public Operator
+{
+private:
   std::function<int(int, int)> fn;
 public:
   BinaryOperator() = delete;
-  BinaryOperator(string name, std::function<int(int, int)> fn);
-  int peform(int a, int b) const;
-  string get_name() const;
+  BinaryOperator(string name, string symbol, std::function<int(int, int)> fn);
+  int perform(int a, int b) const;
 };
 
-class UnaryOperator
+class UnaryOperator : public Operator
 {
 private:
-  string name;
   std::function<int(int)> fn;
 public:
   UnaryOperator() = delete;
-  UnaryOperator(string name, std::function<int(int)> fn);
+  UnaryOperator(string name, string symbol, std::function<int(int)> fn);
   int perform(int a) const;
-  string get_name() const;
 };
--- src/operators.cpp
+++ src/operators.cpp
@@ -2,7 +2,29 @@
 
 
 //put your precedence here for now
-std::vector<string> precedence = {"*", "/", "+", "-"};
+//std::vector<string> precedence = {"*", "/", "+", "-"};
 
 
+Operator::Operator(string name, string symbol) : name(name), symbol(symbol) {}
 
+string Operator::get_name() const
+{
+  return name;
+}
+string Operator::get_symbol() const
+{
+  return symbol;
+}
+
+
+BinaryOperator::BinaryOperator(string name, string symbol, std::function<int(int, int)> fn) : Operator(name, symbol), fn(fn) {}
+int BinaryOperator::perform(int a, int b) const
+{
+  return fn(a, b);
+}
+
+UnaryOperator::UnaryOperator(string name, string symbol, std::function<int(int)> fn) : Operator(name, symbol), fn(fn) {}
+int UnaryOperator::perform(int a) const
+{
+  return fn(a);
+}
