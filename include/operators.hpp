89e094252c172a933cc0778e9244bbd071817337
malevitzch <malevitzch@gmail.com>
2024-07-06 11:02:54 +0200

    Added precedence and Associativity getter to Operator class

--- include/operators.hpp
+++ include/operators.hpp
@@ -1,6 +1,8 @@
 #include <functional>
 #include <vector>
 #include <string>
+#include <map>
+
 using std::string;
 
 enum class Associativity
@@ -12,15 +14,18 @@ enum class Associativity
 class Operator
 {
 private:
-  Associativity associativity;
   string name;
   string symbol;
+  size_t precedence; //the lower precedence the earlier the operator is processed
+  Associativity associativity;
 protected:
-  Operator(string name, string symbol, Associativity associativity);
+  Operator(string name, string symbol, size_t precedence, Associativity associativity);
 public:
   Operator() = delete;
   string get_name() const;
   string get_symbol() const;
+  size_t get_precedence() const;
+  Associativity get_associativity() const;
 };
 class BinaryOperator : public Operator
 {
@@ -28,7 +33,7 @@ private:
   std::function<int(int, int)> fn;
 public:
   BinaryOperator() = delete;
-  BinaryOperator(string name, string symbol, Associativity associativity, std::function<int(int, int)> fn);
+  BinaryOperator(string name, string symbol, size_t precedence, Associativity associativity, std::function<int(int, int)> fn);
   int perform(int a, int b) const;
 };
 
@@ -38,6 +43,8 @@ private:
   std::function<int(int)> fn;
 public:
   UnaryOperator() = delete;
-  UnaryOperator(string name, string symbol, Associativity associativity, std::function<int(int)> fn);
+  UnaryOperator(string name, string symbol, size_t precedence, Associativity associativity, std::function<int(int)> fn);
   int perform(int a) const;
 };
+
+std::map<string, BinaryOperator> get_binary_operator_map(std::vector<BinaryOperator> operator_vector);
--- src/operators.cpp
+++ src/operators.cpp
@@ -1,6 +1,5 @@
 #include "../include/operators.hpp"
-
-Operator::Operator(string name, string symbol, Associativity associativity) : name(name), symbol(symbol), associativity(associativity) {}
+Operator::Operator(string name, string symbol, size_t precedence, Associativity associativity) : name(name), symbol(symbol), precedence(precedence), associativity(associativity) {}
 
 string Operator::get_name() const
 {
@@ -10,15 +9,24 @@ string Operator::get_symbol() const
 {
   return symbol;
 }
-
-BinaryOperator::BinaryOperator(string name, string symbol, Associativity associativity, std::function<int(int, int)> fn) : Operator(name, symbol, associativity), fn(fn) {}
+size_t Operator::get_precedence() const
+{
+  return precedence;
+}
+Associativity Operator::get_associativity() const
+{
+  return associativity;
+}
+BinaryOperator::BinaryOperator(string name, string symbol, size_t precedence, Associativity associativity, std::function<int(int, int)> fn) : Operator(name, symbol, precedence, associativity), fn(fn) {}
 int BinaryOperator::perform(int a, int b) const
 {
   return fn(a, b);
 }
 
-UnaryOperator::UnaryOperator(string name, string symbol,  Associativity associativity, std::function<int(int)> fn) : Operator(name, symbol, associativity), fn(fn) {}
+UnaryOperator::UnaryOperator(string name, string symbol, size_t precedence, Associativity associativity, std::function<int(int)> fn) : Operator(name, symbol, precedence, associativity), fn(fn) {}
 int UnaryOperator::perform(int a) const
 {
   return fn(a);
 }
+
+
