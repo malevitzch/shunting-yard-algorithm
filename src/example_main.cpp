c6eee7ef58f894c696b358994c464959724f6cb2
malevitzch <malevitzch@gmail.com>
2024-07-13 13:33:40 +0200

    implemented operators for testing purposes

--- src/example_main.cpp
+++ src/example_main.cpp
@@ -2,19 +2,25 @@
 #include "../include/simple-linter.hpp"
 #include <iostream>
 
-int add(int a, int b)
+std::vector<BinaryOperator> get_binary_operators()
 {
-  return a+b;
+  std::vector<BinaryOperator> operator_vec;
+  operator_vec.push_back(BinaryOperator("+", "Addition", 2, Associativity::Left, [](int a, int b){return a+b;}));
+  operator_vec.push_back(BinaryOperator("-", "Subtraction", 2, Associativity::Left, [](int a, int b){return a-b;}));
+  operator_vec.push_back(BinaryOperator("*", "Multiplication", 1, Associativity::Left, [](int a, int b){return a*b;}));
+  operator_vec.push_back(BinaryOperator("/", "Division", 2, Associativity::Left, [](int a, int b){return a/b;}));
+  return operator_vec;
 }
-int neg(int a)
+std::vector<UnaryOperator> get_unary_operators()
 {
-  return a*-1;
+  std::vector<UnaryOperator> operator_vec;
+  operator_vec.push_back(UnaryOperator("-", "Signchange", [](int a){return a*-1;}));
+  return operator_vec;
 }
 int main()
 {
-  BinaryOperator addition("+", "Addition", 2, Associativity::Left, add);
-  //int x(7), y(8);
-  //std::cout<<addition.perform(x, y) << "\n";
+  std::vector<UnaryOperator> unary_ops = get_unary_operators();
+  std::vector<BinaryOperator> binary_ops = get_binary_operators();
   std::vector<string> tokenized_expr = tokenize_expression("x8+7--31 * (8 / -217)");
   for(string s : tokenized_expr)
   {
